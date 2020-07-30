25fcba8c09135e7d9430d36c96533d183739deac
HKnaomi <51732480+HKnaomi@users.noreply.github.com>
2020-07-30 23:27:55 +0700

    A

--- Badvpn.sh
+++ Badvpn.sh
@@ -1,20 +1,15 @@
-# configure rc.local
-cat <<EOF >/etc/rc.local
-#!/bin/sh -e
-#
-# rc.local
-#
-# This script is executed at the end of each multiuser runlevel.
-# Make sure that the script will "exit 0" on success or any other
-# value on error.
-#
-# In order to enable or disable this script just change the execution
-# bits.
-#
-# By default this script does nothing.
-
-exit 0
-EOF
-chmod +x /etc/rc.local
-systemctl daemon-reload
-systemctl start rc-local
+# install badvpn
+cd
+apt-get -y install cmake make gcc libc6-dev
+wget https://raw.githubusercontent.com/janda09/private/master/badvpn-1.999.128.tar.bz2
+tar xf badvpn-1.999.128.tar.bz2
+mkdir badvpn-build
+cd badvpn-build
+cmake badvpn-1.999.128 -DBUILD_NOTHING_BY_DEFAULT=1 -DBUILD_UDPGW=1
+make install
+echo 'badvpn-udpgw --listen-addr 127.0.0.1:7500 > /dev/nul &' >> /etc/rc.local
+badvpn-udpgw --listen-addr 127.0.0.1:7500 > /dev/nul &
+cd /usr/bin
+wget https://raw.githubusercontent.com/janda09/private/master/badvpn-udpgw
+chmod 755 badvpn-udpgw
+cd
