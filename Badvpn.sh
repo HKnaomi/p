4fae109a6bda228fa619eb0e1aad84d0fe54ebaa
HKnaomi <51732480+HKnaomi@users.noreply.github.com>
2020-07-30 22:23:37 +0700

    Create Badvpn.sh

---
+++ Badvpn.sh
@@ -0,0 +1,8 @@
+cd
+wget -O /usr/bin/badvpn-udpgw "https://raw.githubusercontent.com/kholizsivoi/script/master/badvpn-udpgw"
+if [ "$OS" == "x86_64" ]; then
+  wget -O /usr/bin/badvpn-udpgw "https://raw.githubusercontent.com/kholizsivoi/script/master/badvpn-udpgw64"
+fi
+sed -i '$ i\screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300' /etc/rc.local
+chmod +x /usr/bin/badvpn-udpgw
+screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300
